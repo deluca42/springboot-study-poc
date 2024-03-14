520bf0381203aa4facf365af7d60ccf7ee73301c
deluca42 <deluca42@gmail.com>
2024-03-14 16:25:36 -0300

    spring initializer

---
+++ src/main/java/net/study/springbootstudypoc/SpringbootStudyPocApplication.java
@@ -0,0 +1,13 @@
+package net.study.springbootstudypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/study/springbootstudypoc/SpringbootStudyPocApplicationTests.java
@@ -0,0 +1,13 @@
+package net.study.springbootstudypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
